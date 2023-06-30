2a96abd2dcb6c401b355e8e33e19a0ec1af6dfef
sarapdelpi <sarapdelpi@gmail.com>
2023-06-30 12:36:53 +0200

    lecutra json exitosa

--- BACK_twitter/src/main/java/com/ssp/Twitter2/Twitter2Application.java
+++ BACK_twitter/src/main/java/com/ssp/Twitter2/Twitter2Application.java
@@ -3,10 +3,18 @@ package com.ssp.Twitter2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class Twitter2Application {
 
 	public static void main(String[] args) {
+
+		ArrayList<Tweet> Datas = new ArrayList<>();
+		LectorJson lector = new LectorJson();
+		Datas = lector.LecturaJSONsimple();
+		System.out.println(Datas);
+
 		SpringApplication.run(Twitter2Application.class, args);
 	}
 
