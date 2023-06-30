fbd546c6f1873fe9d69c5a075d8eab84dc886355
sarapdelpi <sarapdelpi@gmail.com>
2023-06-30 11:43:16 +0200

    subo arquitetura de back

---
+++ BACK_twitter/src/main/java/com/ssp/Twitter2/Twitter2Application.java
@@ -0,0 +1,13 @@
+package com.ssp.Twitter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twitter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Twitter2Application.class, args);
+	}
+
+}
